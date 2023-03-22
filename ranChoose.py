6fec40b9894eeea268308a6cbd22dbbdc9ec4300
Leo Cui <leomyth123@gmail.com>
2023-03-22 11:00:52 -0500

    test

--- ranChoose.py
+++ ranChoose.py
@@ -1,29 +1,9 @@
-# TODO: select specific song to run
-# TODO: lookup on spotify
+# TODO: lookup on mopidy and run
 import json
-import spotipy
-import webbrowser
+import random
   
-username = 'leomyth123@gmail.com'
-clientID = '28493365acaf4b1eaa86768eb221a7a2'
-clientSecret = '6babca25098a4a30bdc6b9dc32463cc8'
-redirect_uri = 'http://google.com/callback/'
-oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirect_uri)
-token_dict = oauth_object.get_access_token()
-token = token_dict['access_token']
-spotifyObject = spotipy.Spotify(auth=token)
-user_name = spotifyObject.current_user()
-  
-# To print the JSON response from 
-# browser in a readable format.
-# optional can be removed
-print(json.dumps(user_name, sort_keys=True, indent=4))
-
-def playSong(search_song):
-    results = spotifyObject.search(search_song, 1, 0, "track")
-    songs_dict = results['tracks']
-    song_items = songs_dict['items']
-    song = song_items[0]['external_urls']['spotify']
-    webbrowser.open(song)
-
-playSong("I Wanna Cry Seori")
+def randomChoose():
+    with open('songCollection.json') as json_file_org:
+        condensed = json.load(json_file_org)
+        name,date = random.choice(list(condensed.items()))
+        return (name,date)
--- spotifyData.py
+++ spotifyData.py
@@ -1,12 +1,26 @@
-# TODO: implement database based off of spotify zip file
-## relevant fields: timestamp, track_name, uri?
-## x: skipped, repeated, ms_played > 60 seconds
 import json
 
-# TODO: Load current data, save. open endsong.json, add to current data, rerwrite to songCollection.json
-
-
-condensed = {}
 # Opening JSON file
-with open('endsong.json') as json_file:
-    rawdata = json.load(json_file)
+with open('songCollection.json') as json_file_org:
+    condensed = json.load(json_file_org)
+    with open('endsong.json') as json_file:
+        rawdata = json.load(json_file)
+        for entry in rawdata:
+            if (not entry["skipped"]) and (entry["ms_played"] > 6000) and (entry["master_metadata_track_name"] not in condensed):
+                condensed[entry["master_metadata_track_name"]] = (entry["master_metadata_album_artist_name"],entry["ts"])
+
+    json_object = json.dumps(condensed, indent=4)
+    with open('songCollection.json','w') as json_file_write:
+        json_file_write.write(json_object)
+
+def addNewSong(name,artist,date):
+    with open('songCollection.json') as json_file_org:
+        condensed = json.load(json_file_org)
+        if name in condensed:
+            return -1
+        else:
+            condensed[name] = (artist,date)
+            json_object = json.dumps(condensed, indent=4)
+            with open('songCollection.json','w') as json_file_write:
+                json_file_write.write(json_object)
+            return 1
