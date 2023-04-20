0518752286bf2a50fd4e6e26687cc7469ff1f4fc
Leo Cui <leomyth123@gmail.com>
2023-04-20 18:23:34 -0500

    display first

--- timingOlly.py
+++ timingOlly.py
@@ -2,15 +2,32 @@
 
 import time
 import random
-# import os
+import sys
 from ranChoose import randomChoose
-from mpd import MPDClient
+# from mpd import MPDClient
+from PyQt5.QtWidgets import * 
+from PyQt5 import QtCore
+from PyQt5 import QtGui
 
 # figure out what terminal command to set up
 
 start = time.time()
 
 PERIOD_OF_TIME = 50400 # 3600=1hr, this is currently set as 14 hours
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # set the title
+        self.setWindowTitle("MemoryBox")
+        # setting  the geometry of window
+        self.setGeometry(0, 0, 400, 300)
+
+        self.timeLeft = QLabel("00:00")
+        self.setCentralWidget(self.timeLeft)
+
+        self.authorTitle = QLabel("Woo - Someone")
+        self.setCentralWidget(self.authorTitle)
 
 while True :
     # time.sleep(600.0 - ((time.time() - start) % 600.0))
@@ -21,10 +38,10 @@ while True :
         while (name == 'null'):
             name,artist,date = randomChoose()
 
-        client = MPDClient()
-        client.timeout = 120
-        client.idletimeout = 120
-        client.connect("localhost", 6600)
+        # client = MPDClient()
+        # client.timeout = 120
+        # client.idletimeout = 120
+        # client.connect("localhost", 6600)
         # print(client.mpd_version)
         # client.clear()
         # client.add("yt:https://youtu.be/7PR3I23cd4I")
@@ -33,6 +50,13 @@ while True :
         # client.disconnect()
 
         # time.sleep(0.3)
+
+        
+        App = QApplication(sys.argv)
+        
+        window = Window()
+        window.show()
+        App.exec()
 
 
         print("success")
