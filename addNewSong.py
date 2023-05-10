0e9f8bd86bfb5efdaef0284525d165ca9082c235
totoroisls2ve <totoroisls2ve@gmail.com>
2023-05-10 00:32:59 -0500

    unicode attempt 2 addNewSong.py

--- addNewSong.py
+++ addNewSong.py
@@ -3,8 +3,8 @@ import json
 flag = True
 with open('/home/memorybox/Desktop/spotify/songCollection.json') as json_file_org:
     condensed = json.load(json_file_org)
-    possibleNew = input("insert new song via in the following form: name,artist,date (without spaces)\n").encode('utf-8').decode('unicode-escape')
-    possibleArr = possibleNew.split(",")
+    possibleNew = input("insert new song via in the following form: name,artist,date (without spaces)\n")
+    possibleArr = possibleNew.encode('utf-8').decode('unicode-escape').split(",")
     if possibleArr[0] in condensed:
         if condensed[possibleArr[0]][0] == possibleArr[1]:
             print("Error: Song already in")
